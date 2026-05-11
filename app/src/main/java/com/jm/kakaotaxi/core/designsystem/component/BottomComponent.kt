9dc9d453195691f0eae5488d2401f0e330a6767e
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-05-12 01:05:50 +0900

    [feat/#9] 바텀 시트의 기본 구조 구현

---
+++ app/src/main/java/com/jm/kakaotaxi/core/designsystem/component/BottomComponent.kt
@@ -0,0 +1,69 @@
+package com.jm.kakaotaxi.core.designsystem.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.jm.kakaotaxi.core.designsystem.theme.KakaotaxiTheme
+import com.jm.kakaotaxi.core.designsystem.theme.KakaotaxiTheme.colors
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun KakaotaxiBottomSheet(
+    onDismissRequest: () -> Unit,
+    modifier: Modifier = Modifier,
+    bottomSheetState: SheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = true,
+    )
+) {
+    ModalBottomSheet(
+        onDismissRequest = onDismissRequest,
+        modifier = modifier,
+        sheetState = bottomSheetState,
+        sheetMaxWidth = Dp.Unspecified,
+        shape = RoundedCornerShape(
+            topStart = 20.dp,
+            topEnd = 20.dp,
+        ),
+        containerColor = colors.white,
+        scrimColor = colors.subGray2,
+        dragHandle = { CustomDragHandle() },
+    ) {}
+}
+
+@Composable
+private fun CustomDragHandle(
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier
+            .padding(vertical = 8.dp)
+            .size(width = 44.dp, height = 4.dp)
+            .background(
+                color = colors.subGray2,
+                shape = RoundedCornerShape(28.0.dp)
+            ),
+    )
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+private fun KakaoTaxiBottomSheetPreview() {
+    KakaotaxiTheme {
+        KakaotaxiBottomSheet(
+            onDismissRequest = {},
+        )
+    }
+}
