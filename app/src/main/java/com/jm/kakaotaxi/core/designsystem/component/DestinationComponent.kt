47f62cf7f77ed4dde90f88ce48465ed612de75ee
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-05-13 00:36:53 +0900

    [del/#10] 불필요한 import문 제거

--- app/src/main/java/com/jm/kakaotaxi/core/designsystem/component/DestinationComponent.kt
+++ app/src/main/java/com/jm/kakaotaxi/core/designsystem/component/DestinationComponent.kt
@@ -1,13 +1,8 @@
 package com.jm.kakaotaxi.core.designsystem.component
 
-import android.R.attr.contentDescription
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -19,7 +14,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -83,10 +77,6 @@ fun DestinationItem() {
 @Composable
 private fun KakaoTaxiDestinationItemPreview() {
     KakaotaxiTheme {
-        Column(
-            modifier = Modifier.padding(16.dp)
-        ) {
-           DestinationItem()
-        }
+        DestinationItem()
     }
 }
