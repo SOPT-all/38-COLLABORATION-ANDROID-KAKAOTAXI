8261102305d5d21c836da4af0b70fc4bc8cde396
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-05-13 16:11:27 +0900

    [mod/#10] 크기 대응을 위해 size 제거 후 fillMaxWidth로 대체

--- app/src/main/java/com/jm/kakaotaxi/core/designsystem/component/DestinationComponent.kt
+++ app/src/main/java/com/jm/kakaotaxi/core/designsystem/component/DestinationComponent.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -30,7 +31,7 @@ fun DestinationItem(
 ) {
     Row(
         modifier = modifier
-            .size(320.dp, 43.dp)
+            .fillMaxWidth()
             .shadow(
                 elevation   = 10.dp,
                 shape       = RoundedCornerShape(50.dp),
