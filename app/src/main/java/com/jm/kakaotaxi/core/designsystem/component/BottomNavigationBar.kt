48d94ed08e0cc825765b426f6a131e63c614a2d4
chosumin432 <01074260518k@gmail.com>
2026-05-14 03:02:59 +0900

    [mod/#11] BottomNavigationBar 코드 리뷰 재수정

--- app/src/main/java/com/jm/kakaotaxi/core/designsystem/component/BottomNavigationBar.kt
+++ app/src/main/java/com/jm/kakaotaxi/core/designsystem/component/BottomNavigationBar.kt
@@ -1,9 +1,11 @@
 package com.jm.kakaotaxi.core.designsystem.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -13,61 +15,74 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jm.kakaotaxi.R
+import com.jm.kakaotaxi.core.designsystem.theme.KakaotaxiTheme
 
 @Composable
 fun BottomNavigationBar(
+    selectedIndex: Int,
+    onSelectedChange: (Int) -> Unit,
+    modifier: Modifier = Modifier
 ){
-    var selectedIndex by remember {
-        mutableStateOf(-1)
-    }
     Row(
         modifier = Modifier
-            .padding(start = 25.dp, end = 24.dp)
+            .background(
+                color = KakaotaxiTheme.colors.white,
+                shape = RoundedCornerShape(
+                    topStart = 20.dp,
+                    topEnd = 29.dp
+                )
+            )
+            .padding(horizontal = 25.dp)
             .padding(top = 8.dp, bottom = 2.dp),
         horizontalArrangement = Arrangement.spacedBy(5.dp)
     ){
         BottomNavItem(
+            modifier = Modifier.weight(1f),
             icon = R.drawable.ic_homenavigation_menu,
             label = "전체보기",
             isSelected = selectedIndex == 0,
             onClick = {
-                selectedIndex = 0
+                onSelectedChange (0)
             }
         )
 
         BottomNavItem(
+            modifier = Modifier.weight(1f),
             icon = R.drawable.ic_homenavigation_business,
             label = "비즈니스",
             isSelected = selectedIndex == 1,
             onClick = {
-                selectedIndex = 1
+                onSelectedChange(1)
             }
         )
 
         BottomNavItem(
+            modifier = Modifier.weight(1f),
             icon = R.drawable.ic_homenavigation_house,
             label = "홈",
             isSelected = selectedIndex == 2,
             onClick = {
-                selectedIndex = 2
+                onSelectedChange(2)
             }
         )
 
         BottomNavItem(
+            modifier = Modifier.weight(1f),
             icon = R.drawable.ic_homenavigation_bell,
             label = "이용/알림",
             isSelected = selectedIndex == 3,
             onClick = {
-                selectedIndex = 3
+                onSelectedChange(3)
             }
         )
 
         BottomNavItem(
+            modifier = Modifier.weight(1f),
             icon = R.drawable.ic_homenavigation_user,
             label = "내 정보",
             isSelected = selectedIndex == 4,
             onClick = {
-                selectedIndex = 4
+                onSelectedChange(4)
             }
         )
     }
@@ -75,6 +90,14 @@ fun BottomNavigationBar(
 
 @Preview(showBackground = true)
 @Composable
-fun BottomNavigationBarPreview() {
-    BottomNavigationBar()
+private fun BottomNavigationBarPreview() {
+         var selectedIndex by remember {
+        mutableStateOf(0)
+        }
+    BottomNavigationBar(
+        selectedIndex = selectedIndex,
+        onSelectedChange = {
+            selectedIndex = it
+        }
+    )
 }
