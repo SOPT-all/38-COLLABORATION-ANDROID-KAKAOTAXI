ac6f8737d91f8504cb5d83aa86c3fba7da60f7f4
chosumin432 <01074260518k@gmail.com>
2026-05-14 16:51:22 +0900

    [feat/#11] 택시 부르기 버튼 생성

---
+++ app/src/main/java/com/jm/kakaotaxi/core/designsystem/component/TaxiCallButton.kt
@@ -0,0 +1,35 @@
+package com.jm.kakaotaxi.core.designsystem.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jm.kakaotaxi.core.designsystem.theme.KakaotaxiTheme
+
+@Composable
+fun TaxiCallButton(
+    modifier: Modifier = Modifier
+) {
+    ButtonItem(
+        modifier = modifier.padding(horizontal = 19.dp),
+        text = "택시 부르기",
+        backgroundColor = KakaotaxiTheme.colors.primaryBlue,
+        contentColor = KakaotaxiTheme.colors.white,
+        paddingValues = PaddingValues(
+            horizontal = 117.75.dp,
+            vertical = 12.5.dp
+        ),
+        onClick = {}
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TaxiCallButtonPreview() {
+    KakaotaxiTheme {
+        TaxiCallButton()
+    }
+}
