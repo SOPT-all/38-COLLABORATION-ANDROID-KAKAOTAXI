ebdd603718b8fb02bb45c17c42f3ddf30c6f4ac2
chldmswll <cuicui633@naver.com>
2026-05-14 17:33:45 +0900

    [mod/#8] VerticalDivider height 수정

--- app/src/main/java/com/jm/kakaotaxi/presentation/home/component/NoticeSection.kt
+++ app/src/main/java/com/jm/kakaotaxi/presentation/home/component/NoticeSection.kt
@@ -1,16 +1,10 @@
 package com.jm.kakaotaxi.presentation.home.component
 
-import android.graphics.Color
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
@@ -18,60 +12,57 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.modifier.modifierLocalConsumer
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jm.kakaotaxi.R
-import com.jm.kakaotaxi.core.designsystem.theme.KakaoColors
 import com.jm.kakaotaxi.core.designsystem.theme.KakaotaxiTheme
 
 @Composable
 fun NoticeSection(
     modifier: Modifier = Modifier
-){
+) {
     Row(
         modifier = modifier
             .fillMaxWidth()
             .padding(vertical = 3.dp),
         verticalAlignment = Alignment.CenterVertically
-        ){
-            Text(
-                text = "공지",
-                style = KakaotaxiTheme.typography.body.kakaoB14,
-                color = KakaotaxiTheme.colors.black
-            )
+    ) {
+        Text(
+            text = "공지",
+            style = KakaotaxiTheme.typography.body.kakaoB14,
+            color = KakaotaxiTheme.colors.black
+        )
 
-            Spacer(modifier = Modifier.weight(1f))
+        Spacer(modifier = Modifier.weight(1f))
 
-            VerticalDivider(
-                modifier = Modifier
-                    .padding(vertical = 5.dp),
-                thickness = 1.dp,
-                color = KakaotaxiTheme.colors.backgroundGray
-            )
+        VerticalDivider(
+            modifier = Modifier
+                .height(11.dp),
+            thickness = 1.dp,
+            color = KakaotaxiTheme.colors.backgroundGray
+        )
 
-            Spacer(modifier = Modifier.weight(1f))
+        Spacer(modifier = Modifier.weight(1f))
 
-            Text(
-                text = "[이벤트] 카카오 T 멤버스 5월 쿠폰 이벤트!",
-                style = KakaotaxiTheme.typography.body.kakaoR13,
-                color= KakaotaxiTheme.colors.black
-            )
+        Text(
+            text = "[이벤트] 카카오 T 멤버스 5월 쿠폰 이벤트!",
+            style = KakaotaxiTheme.typography.body.kakaoR13,
+            color = KakaotaxiTheme.colors.black
+        )
 
-            Spacer(modifier = Modifier.weight(1f))
+        Spacer(modifier = Modifier.weight(1f))
 
-            Icon(
-                imageVector = ImageVector.vectorResource(R.drawable.ic_home_notice_more),
-                contentDescription = null,
-                modifier = Modifier.padding(horizontal = 6.dp, vertical = 7.dp)
-            )
-        }
+        Icon(
+            imageVector = ImageVector.vectorResource(R.drawable.ic_home_notice_more),
+            contentDescription = null,
+            modifier = Modifier.padding(horizontal = 6.dp, vertical = 7.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-private fun NoticeSectionPreview(){
+private fun NoticeSectionPreview() {
     NoticeSection()
 }
