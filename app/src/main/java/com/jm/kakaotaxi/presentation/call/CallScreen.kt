eee0da0942821f01eb5baebb124a082264775a9b
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-05-15 16:36:49 +0900

    [del/#22] 불필요한 import문 추가

--- app/src/main/java/com/jm/kakaotaxi/presentation/call/CallScreen.kt
+++ app/src/main/java/com/jm/kakaotaxi/presentation/call/CallScreen.kt
@@ -4,7 +4,6 @@ import android.R.attr.end
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
