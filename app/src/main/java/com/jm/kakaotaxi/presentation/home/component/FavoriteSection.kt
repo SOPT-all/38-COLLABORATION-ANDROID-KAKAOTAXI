ad18e579db6cbbdc2e873bb877e8dc1d3849aaad
chldmswll <cuicui633@naver.com>
2026-05-17 06:25:02 +0900

    [del/#23] 미사용 import 제거

--- app/src/main/java/com/jm/kakaotaxi/presentation/home/component/FavoriteSection.kt
+++ app/src/main/java/com/jm/kakaotaxi/presentation/home/component/FavoriteSection.kt
@@ -1,6 +1,5 @@
 package com.jm.kakaotaxi.presentation.home.component
 
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,7 +7,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -28,16 +26,16 @@ fun FavoriteSection(
     services: ImmutableList<FavoriteServiceModel>,
     onStarClick: (Int) -> Unit,
     modifier: Modifier = Modifier
-){
+) {
     Column(
         modifier = modifier
-    ){
+    ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
-        ){
+        ) {
             Text(
                 text = "자주 쓰는 서비스",
                 color = KakaotaxiTheme.colors.textPrimary,
