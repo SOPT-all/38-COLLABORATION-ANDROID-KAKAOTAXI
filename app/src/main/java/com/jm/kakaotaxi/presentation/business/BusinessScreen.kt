419fcdfb567ade5a009da82134e2fe93b87fb9e0
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-05-18 03:00:58 +0900

    [add/#29] 미사용 MainTab 스크린 추가

---
+++ app/src/main/java/com/jm/kakaotaxi/presentation/business/BusinessScreen.kt
@@ -0,0 +1,33 @@
+package com.jm.kakaotaxi.presentation.business
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BusinessRoute(
+    modifier: Modifier = Modifier,
+) {
+    BusinessScreen(
+        modifier = modifier,
+    )
+}
+
+@Composable
+private fun BusinessScreen(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text("Business")
+    }
+}
---
+++ app/src/main/java/com/jm/kakaotaxi/presentation/menu/MenuScreen.kt
@@ -0,0 +1,33 @@
+package com.jm.kakaotaxi.presentation.menu
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MenuRoute(
+    modifier: Modifier = Modifier,
+) {
+    MenuScreen(
+        modifier = modifier,
+    )
+}
+
+@Composable
+private fun MenuScreen(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text("Menu")
+    }
+}
