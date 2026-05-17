75120984d254622e571b23698f48b29780f450b7
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-05-18 00:41:46 +0900

    [feat/#22] 택시 호출  화면 Preview 제작

--- app/src/main/java/com/jm/kakaotaxi/presentation/call/component/TaxiSelectContent.kt
+++ app/src/main/java/com/jm/kakaotaxi/presentation/call/component/TaxiSelectContent.kt
@@ -12,12 +12,17 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jm.kakaotaxi.R
 import com.jm.kakaotaxi.core.designsystem.component.ButtonItem
@@ -26,6 +31,7 @@ import com.jm.kakaotaxi.core.designsystem.theme.KakaotaxiTheme
 import com.jm.kakaotaxi.core.designsystem.theme.KakaotaxiTheme.colors
 import com.jm.kakaotaxi.data.model.call.TaxiInfoModel
 import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 
 @Composable
 fun TaxiSelectContent(
@@ -97,6 +103,51 @@ fun TaxiSelectContent(
                 onCallClick()
             },
             modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TaxiSelectContentPreview() {
+    KakaotaxiTheme() {
+        val taxiServices = persistentListOf(
+            TaxiInfoModel(
+                id = 1,
+                taxiImage = R.drawable.img_calltaxi_safetaxi,
+                taxiType = "안심 택시",
+                taxiDescription = "친절한 기사님\n바로 배차 보장",
+                taxiPrice = "7,800",
+            ),
+            TaxiInfoModel(
+                id = 2,
+                taxiImage = R.drawable.img_calltaxi_gentaxi,
+                taxiType = "일반 택시",
+                taxiDescription = "가장 빠르게\n주변 택시 연결",
+                taxiPrice = "4,800",
+            ),
+            TaxiInfoModel(
+                id = 3,
+                taxiImage = R.drawable.img_calltaxi_greattaxi,
+                taxiType = "모범 택시",
+                taxiDescription = "고급 차량\n베테랑 기사님",
+                taxiPrice = "10,000",
+            ),
+            TaxiInfoModel(
+                id = 4,
+                taxiImage = R.drawable.img_calltaxi_bigtaxi,
+                taxiType = "대형 택시",
+                taxiDescription = "넓은 좌석\n짐이 많을 때",
+                taxiPrice = "13,000",
+            )
+        )
+        var selectedTaxi by remember { mutableStateOf(taxiServices.first()) }
+
+        TaxiSelectContent(
+            taxiInfo = taxiServices,
+            selectedTaxi = selectedTaxi,
+            onServiceChange = { selectedTaxi = it },
+            onCallClick = {}
         )
     }
 }
