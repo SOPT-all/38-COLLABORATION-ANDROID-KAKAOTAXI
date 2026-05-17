d5f3b16ec25e86f906a6a829095a73d8ec998de0
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-05-18 00:53:00 +0900

    [feat/#22] 목적지 확인 화명네 딤 처리

--- app/src/main/java/com/jm/kakaotaxi/presentation/call/CallScreen.kt
+++ app/src/main/java/com/jm/kakaotaxi/presentation/call/CallScreen.kt
@@ -1,6 +1,7 @@
 package com.jm.kakaotaxi.presentation.call
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -12,6 +13,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -61,12 +63,20 @@ private fun CallScreen(
             contentScale = ContentScale.Crop,
             modifier = modifier.fillMaxSize()
         )
-
+        
         DestinationItem(
             modifier = Modifier
                 .align(Alignment.TopCenter)
                 .padding(start = 20.dp, top = 44.dp, end = 20.dp)
         )
+
+        if (bottomSheetStep == 1) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0x66000000))
+            )
+        }
 
         KakaoTaxiBottomSheet(
             onDismissRequest = {},
