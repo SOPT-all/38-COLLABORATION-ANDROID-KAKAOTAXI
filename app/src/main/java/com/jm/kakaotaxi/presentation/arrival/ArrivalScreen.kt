43a8887dc90dcf5406d69d18d761ba83219310a4
chosumin432 <01074260518k@gmail.com>
2026-05-17 17:12:51 +0900

    [feat/#11] 도착 화면 뷰 구현

--- app/src/main/java/com/jm/kakaotaxi/presentation/arrival/ArrivalScreen.kt
+++ app/src/main/java/com/jm/kakaotaxi/presentation/arrival/ArrivalScreen.kt
@@ -1,8 +1,42 @@
 package com.jm.kakaotaxi.presentation.arrival
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.BiasAbsoluteAlignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jm.kakaotaxi.R
+import com.jm.kakaotaxi.core.designsystem.component.ButtonItem
+import com.jm.kakaotaxi.core.designsystem.component.ButtonStyle
+import com.jm.kakaotaxi.core.designsystem.component.KakaoTaxiBottomSheet
 import com.jm.kakaotaxi.core.designsystem.theme.KakaotaxiTheme
 
 @Composable
@@ -14,11 +48,203 @@ fun ArrivalRoute(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun ArrivalScreen(
     modifier: Modifier = Modifier
 ) {
+    Box(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(R.drawable.img_arrival_map),
+            contentDescription = null,
+            contentScale = ContentScale.Crop
+        )
 
+        Spacer(modifier = modifier.padding(top = 191.dp))
+
+        Icon(
+            painter = painterResource(R.drawable.ic_arrivescreen_gps),
+            contentDescription = null,
+            tint = Color.Unspecified
+        )
+
+        KakaoTaxiBottomSheet {
+            Column(modifier = modifier.padding(horizontal = 21.dp))
+            {
+                Spacer(modifier = modifier.height(16.dp))
+
+                Box{
+                    Image(
+                        painter = painterResource(R.drawable.img_arrival_taxi),
+                        contentDescription = null
+                    )
+
+                    Icon(
+                        painter = painterResource(R.drawable.ic_arrivescreen_arrowsout),
+                        contentDescription = null,
+                        tint = Color.Unspecified,
+                        modifier = modifier
+                            .align(Alignment.BottomEnd)
+                            .padding(end = 15.dp, bottom = 13.dp)
+                    )
+                }
+
+                Spacer(modifier = modifier.height(31.dp))
+
+                Row {
+                    Text(
+                        text = "안심택시",
+                        style = KakaotaxiTheme.typography.body.kakaoB16,
+                        color = KakaotaxiTheme.colors.primaryBlue
+                    )
+
+                    Spacer(modifier = modifier.width(6.dp))
+
+                    Text(
+                        text = "지금 탑승 위치로 출발",
+                        style = KakaotaxiTheme.typography.body.kakaoB16,
+                        color = KakaotaxiTheme.colors.textPrimary
+                    )
+                }
+
+                Spacer(modifier = modifier.height(2.dp))
+
+                Text(
+                    text = "피크요금 1.2배 적용",
+                    style = KakaotaxiTheme.typography.label.kakaoR12,
+                    color = KakaotaxiTheme.colors.textSecondary
+                )
+
+                Spacer(modifier = Modifier.height(15.dp))
+
+                HorizontalDivider(
+                    thickness = 1.dp,
+                    color = KakaotaxiTheme.colors.backgroundGray
+                )
+
+                Row {
+                    Image(
+                        painter = painterResource(R.drawable.img_arrival_driverprofile),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(top = 20.dp, bottom = 21.dp)
+                            .size(size = 55.dp)
+                    )
+
+                    Spacer(modifier = Modifier.width(7.dp))
+
+                    Column {
+                        Text(
+                            text = "서울 38 아 2864",
+                            modifier = modifier.padding(top = 24.dp),
+                            style = KakaotaxiTheme.typography.body.kakaoB16,
+                            color = KakaotaxiTheme.colors.textPrimary
+                        )
+
+                        Spacer(modifier = Modifier.height(3.dp))
+
+                        Text(
+                            text = "김민수 기사님",
+                            style = KakaotaxiTheme.typography.body.kakaoR14,
+                            color = KakaotaxiTheme.colors.textSecondary
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.width(7.dp))
+
+                    VerticalDivider(
+                        modifier = Modifier
+                            .height(13.dp)
+                            .padding(top = 30.dp),
+                        thickness = 1.dp,
+                        color = KakaotaxiTheme.colors.backgroundGray
+                    )
+
+                    Spacer(modifier = Modifier.width(7.dp))
+
+                    Text(
+                        text = "현대 쏘나타 화이트",
+                        modifier = modifier.padding(top = 24.dp),
+                        style = KakaotaxiTheme.typography.body.kakaoR14,
+                        color = KakaotaxiTheme.colors.textSecondary
+                    )
+                }
+                //전달 메시지
+                Row(
+                    modifier = modifier
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .weight(1f)
+                            .background(
+                                shape = RoundedCornerShape(size = 10.dp),
+                                color = KakaotaxiTheme.colors.backgroundGray2
+                            )
+                            .border(
+                                width = 1.dp,
+                                shape = RoundedCornerShape(size = 10.dp),
+                                color = KakaotaxiTheme.colors.backgroundGray
+                            )
+                            .padding(horizontal = 10.dp, vertical = 4.dp)
+                    ) {
+                        Icon(
+                            imageVector = ImageVector.vectorResource(R.drawable.ic_arrivescreen_checkcircle),
+                            contentDescription = null,
+                            tint = Color.Unspecified
+                        )
+
+                        Spacer(modifier = Modifier.width(4.dp))
+
+                        Text(
+                            text = "천천히 탑승하실 수 있도록 전달해요!",
+                            modifier = Modifier.padding(vertical = 7.dp),
+                            style = KakaotaxiTheme.typography.body.kakaoR14,
+                            color = KakaotaxiTheme.colors.textSecondary
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.width(7.dp))
+                    //전화 버튼
+                    Box(
+                        modifier = Modifier
+                            .background(
+                                shape = RoundedCornerShape(size = 10.dp),
+                                color = KakaotaxiTheme.colors.backgroundGray2
+                            )
+                            .border(
+                                width = 1.dp,
+                                shape = RoundedCornerShape(size = 10.dp),
+                                color = KakaotaxiTheme.colors.backgroundGray
+                            )
+                            .padding(all = 10.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Icon(
+                            imageVector = ImageVector.vectorResource(R.drawable.ic_arrivescreen_phone),
+                            contentDescription = null,
+                            tint = Color.Unspecified
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(20.dp))
+                //공유 버튼
+                ButtonItem(
+                    text = "보호자에게 공유",
+                    style = ButtonStyle.SHARE,
+                    onClick = {},
+                    modifier = modifier.fillMaxWidth()
+                )
+
+                Spacer(modifier = modifier.height(23.dp))
+            }
+
+        }
+    }
 }
 
 
