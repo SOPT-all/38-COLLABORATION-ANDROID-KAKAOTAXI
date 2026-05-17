260d1f5ff12bfab3addb5c5ec7baf318eb945ce1
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-05-18 02:59:59 +0900

    [feat/#29] MainNavHost 추가

---
+++ app/src/main/java/com/jm/kakaotaxi/presentation/main/MainNavHost.kt
@@ -0,0 +1,42 @@
+package com.jm.kakaotaxi.presentation.main
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import com.jm.kakaotaxi.presentation.bell.navigation.bellNavGraph
+import com.jm.kakaotaxi.presentation.business.navigation.businessNavGraph
+import com.jm.kakaotaxi.presentation.home.navigation.homeNavGraph
+import com.jm.kakaotaxi.presentation.menu.navigation.menuNavGraph
+import com.jm.kakaotaxi.presentation.user.navigation.userNavGraph
+
+@Composable
+fun MainNavHost(
+    appState: MainAppState,
+    innerPadding: PaddingValues,
+) {
+    val navController = appState.navController
+
+    NavHost(
+        navController = navController,
+        startDestination = appState.startDestination,
+    ) {
+        menuNavGraph(
+            innerPadding = innerPadding,
+        )
+        businessNavGraph(
+            innerPadding = innerPadding,
+        )
+        homeNavGraph(
+            navController = navController,
+            innerPadding = innerPadding,
+        )
+        bellNavGraph(
+            innerPadding = innerPadding,
+        )
+        userNavGraph(
+            innerPadding = innerPadding,
+        )
+
+        
+    }
+}
