e0b8746b6a3d4ad4a506f50e183da1410e7128c8
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-05-18 03:00:30 +0900

    [mod/#29] BottomNavigationBar에 visible 추가

--- app/src/main/java/com/jm/kakaotaxi/presentation/main/component/BottomNavigationBar.kt
+++ app/src/main/java/com/jm/kakaotaxi/presentation/main/component/BottomNavigationBar.kt
@@ -1,5 +1,11 @@
 package com.jm.kakaotaxi.presentation.main.component
 
+import android.R.attr.visible
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideIn
+import androidx.compose.animation.slideOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
@@ -13,6 +19,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.jm.kakaotaxi.core.designsystem.theme.KakaotaxiTheme
 import kotlinx.collections.immutable.ImmutableList
@@ -20,35 +27,42 @@ import kotlinx.collections.immutable.toImmutableList
 
 @Composable
 fun BottomNavigationBar(
+    visible: Boolean,
     items: ImmutableList<BottomNavType>,
-    selectedItem: BottomNavType,
+    selectedItem: BottomNavType?,
     onItemSelected: (BottomNavType) -> Unit,
     modifier: Modifier = Modifier
 
-){
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .background(
-                color = KakaotaxiTheme.colors.white,
-                shape = RoundedCornerShape(
-                    topStart = 20.dp,
-                    topEnd = 29.dp
+) {
+    AnimatedVisibility(
+        visible = visible,
+        enter = fadeIn() + slideIn { IntOffset(0, it.height) },
+        exit = fadeOut() + slideOut { IntOffset(0, it.height) },
+    ) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .background(
+                    color = KakaotaxiTheme.colors.white,
+                    shape = RoundedCornerShape(
+                        topStart = 20.dp,
+                        topEnd = 29.dp
+                    )
                 )
-            )
-            .padding(horizontal = 25.dp)
-            .padding(top = 8.dp, bottom = 2.dp),
-        horizontalArrangement = Arrangement.spacedBy(5.dp)
-    ){
-        items.forEach {item->
-            BottomNavItem(
-                icon = if (item == selectedItem) item.selectedIconRes else item.unselectedIconRes,
-                label = item. label,
-                isSelected = item == selectedItem,
-                onClick = {onItemSelected(item)},
-                modifier = Modifier.weight(1f)
+                .padding(horizontal = 25.dp)
+                .padding(top = 8.dp, bottom = 2.dp),
+            horizontalArrangement = Arrangement.spacedBy(5.dp)
+        ) {
+            items.forEach { item ->
+                BottomNavItem(
+                    icon = if (item == selectedItem) item.selectedIconRes else item.unselectedIconRes,
+                    label = item.label,
+                    isSelected = item == selectedItem,
+                    onClick = { onItemSelected(item) },
+                    modifier = Modifier.weight(1f)
 
-            )
+                )
+            }
         }
     }
 }
@@ -65,6 +79,7 @@ private fun BottomNavigationBarPreview() {
 
     KakaotaxiTheme {
         BottomNavigationBar(
+            visible = true,
             items = items,
             selectedItem = selectedItem,
             onItemSelected = { selectedItem = it }
