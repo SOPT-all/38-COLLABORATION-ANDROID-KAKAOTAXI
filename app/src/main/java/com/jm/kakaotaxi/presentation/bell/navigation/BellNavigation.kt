21cba80d1630ec7f244c1842e65439796b8a6806
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-05-18 03:22:34 +0900

    [feat/#29] navOptions = null

--- app/src/main/java/com/jm/kakaotaxi/presentation/bell/navigation/BellNavigation.kt
+++ app/src/main/java/com/jm/kakaotaxi/presentation/bell/navigation/BellNavigation.kt
@@ -11,7 +11,7 @@ import com.jm.kakaotaxi.core.navigation.MainTabRoute.Bell
 import com.jm.kakaotaxi.presentation.bell.BellRoute
 
 fun NavController.navigateToBell(
-    navOptions: NavOptions,
+    navOptions: NavOptions? = null,
 ) {
     navigate(Bell, navOptions)
 }
