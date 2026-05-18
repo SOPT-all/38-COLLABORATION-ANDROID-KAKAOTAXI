212d888258fe942d38c477471d6081bf0f4edd5b
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-05-18 21:12:52 +0900

    [mod/#34] header name 수정

--- app/src/main/java/com/jm/kakaotaxi/core/network/TokenInterceptor.kt
+++ app/src/main/java/com/jm/kakaotaxi/core/network/TokenInterceptor.kt
@@ -6,12 +6,12 @@ import okhttp3.Response
 class TokenInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response = with(chain) {
         val newRequest = request().newBuilder()
-            .addHeader(AUTHORIZATION, "1")
+            .addHeader(HEADER_USER_ID, "1")
             .build()
         proceed(newRequest)
     }
 
     companion object {
-        private const val AUTHORIZATION = "X-User-Id"
+        private const val HEADER_USER_ID = "X-User-Id"
     }
 }
