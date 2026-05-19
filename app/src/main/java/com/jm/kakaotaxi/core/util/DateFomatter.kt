e3ad4e493dd8c1f910e096636c35cf409a25795d
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-05-19 14:39:07 +0900

    [chore/#37] DateFormatter 이름 변경

--- app/src/main/java/com/jm/kakaotaxi/core/util/DateFomatter.kt
+++ app/src/main/java/com/jm/kakaotaxi/core/util/DateFomatter.kt
@@ -3,33 +3,27 @@ package com.jm.kakaotaxi.core.util
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
+import java.util.Locale
 
 
 object DateFormatter {
     fun formatDate(dateString: String): String {
-        val formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd'T'HHmmss")
+        val formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd'T'HHmmss", Locale.KOREAN)
 
-        val targetDateTime = LocalDateTime.parse(dateString, formatter)
+        val receivedDate = LocalDateTime.parse(dateString, formatter)
         val now = LocalDateTime.now()
 
-        val days = ChronoUnit.DAYS.between(targetDateTime.toLocalDate(), now.toLocalDate())
-        val months = ChronoUnit.MONTHS.between(
-            targetDateTime.toLocalDate().withDayOfMonth(1),
-            now.toLocalDate().withDayOfMonth(1)
-        )
-        val years = ChronoUnit.YEARS.between(targetDateTime, now)
+        val days = ChronoUnit.DAYS.between(receivedDate, now)
+        val months = ChronoUnit.MONTHS.between(receivedDate, now)
+        val years = ChronoUnit.YEARS.between(receivedDate, now)
 
-        val timeText = if (targetDateTime.hour < 12) "오전" else "오후"
+        val timeText = if (receivedDate.hour < 12) "오전" else "오후"
 
         return when {
             days == 0L -> "오늘 $timeText"
-
             days == 1L -> "어제 $timeText"
-
             days < 30 -> "${days}일 전 $timeText"
-
             months < 12 -> "${months}개월 전 $timeText"
-
             else -> "${years}년 전 $timeText"
         }
     }
