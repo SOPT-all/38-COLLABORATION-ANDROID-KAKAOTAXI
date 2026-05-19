9551144619dc9448ad44bfbe7f98e28bad69acb9
chldmswll <cuicui633@naver.com>
2026-05-19 11:09:42 +0900

    [add/#38] QuickPlaceRemoteDataSource 추가

--- app/src/main/java/com/jm/kakaotaxi/data/remote/datasource/api/QuickPlaceRemoteDatasource.kt
+++ app/src/main/java/com/jm/kakaotaxi/data/remote/datasource/api/QuickPlaceRemoteDatasource.kt
@@ -1,9 +1,8 @@
 package com.jm.kakaotaxi.data.remote.datasource.api
 
 import com.jm.kakaotaxi.data.remote.dto.BaseResponse
-import retrofit2.http.GET
 
-interface QuickPlaceApi {
-    @GET("/v1/places/favorite")
+
+interface QuickPlaceRemoteDatasource {
     suspend fun getQuickPlace(): BaseResponse<List<String>>
 }
