bcbb4e4ee2af86969eb89ccb870acdc674d5dbd1
chosumin432 <01074260518k@gmail.com>
2026-05-20 02:27:26 +0900

    [feat/#39] 수정 사항 반영

--- app/src/main/java/com/jm/kakaotaxi/data/remote/dto/BaseResponse.kt
+++ app/src/main/java/com/jm/kakaotaxi/data/remote/dto/BaseResponse.kt
@@ -14,24 +14,3 @@ data class BaseResponse<T>(
     @SerialName("data")
     val data: T? = null,
 )
-
-@Serializable
-data class T(
-    @SerialName("taxi")
-    val taxi: List<CallInfo>
-){}
-@Serializable
-data class CallInfo(
-    @SerialName("taxi_id")
-    val taxi_id: Int,
-    @SerialName("type")
-    val type: String,
-    @SerialName("model_name")
-    val model_name: String,
-    @SerialName("model_color")
-    val model_color: String,
-    @SerialName("plate_number")
-    val plate_number: String,
-    @SerialName("driver_name")
-    val driver_name: String,
-){}
--- app/src/main/java/com/jm/kakaotaxi/data/remote/service/TaxiCallService.kt
+++ app/src/main/java/com/jm/kakaotaxi/data/remote/service/TaxiCallService.kt
@@ -1,11 +1,14 @@
 package com.jm.kakaotaxi.data.remote.service
 
 import com.jm.kakaotaxi.data.remote.dto.BaseResponse
-import com.jm.kakaotaxi.data.remote.dto.T
-import retrofit2.Response
+import com.jm.kakaotaxi.data.remote.dto.response.CallInfo
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface TaxiCallService {
+
     @GET("api/v1/rides/{taxi_id}")
-    suspend fun callinfoList(): Response<BaseResponse<T>>
+    suspend fun callInfo(
+        @Path("taxi_id") taxiId: Int
+    ): BaseResponse<CallInfo>
 }
