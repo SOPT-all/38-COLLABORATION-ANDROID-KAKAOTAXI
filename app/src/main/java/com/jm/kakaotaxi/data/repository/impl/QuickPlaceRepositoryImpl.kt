52fe2497b2323257e69367a9eafc5b4192d31961
chldmswll <cuicui633@naver.com>
2026-05-19 14:24:27 +0900

    [mod/#38] List<String> -> QuickPlaceListModel 수정

--- app/src/main/java/com/jm/kakaotaxi/data/repository/impl/QuickPlaceRepositoryImpl.kt
+++ app/src/main/java/com/jm/kakaotaxi/data/repository/impl/QuickPlaceRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package com.jm.kakaotaxi.data.repository.impl
 
 import com.jm.kakaotaxi.core.util.suspendRunCatching
+import com.jm.kakaotaxi.data.mapper.toQuickPlaceListModel
+import com.jm.kakaotaxi.data.model.QuickPlaceListModel
 import com.jm.kakaotaxi.data.remote.datasource.api.QuickPlaceRemoteDatasource
 import com.jm.kakaotaxi.data.repository.api.QuickPlaceRepository
 
@@ -8,8 +10,9 @@ class QuickPlaceRepositoryImpl (
     private val quickPlaceRemoteDatasource: QuickPlaceRemoteDatasource
 ) : QuickPlaceRepository {
 
-    override suspend fun getQuickPlace(): Result<List<String>> =
+    override suspend fun getQuickPlace(): Result<QuickPlaceListModel> =
         suspendRunCatching {
-            quickPlaceRemoteDatasource.getQuickPlace().data ?: emptyList()
+            quickPlaceRemoteDatasource.getQuickPlace().data
+                ?.toQuickPlaceListModel() ?: QuickPlaceListModel(emptyList())
         }
 }
