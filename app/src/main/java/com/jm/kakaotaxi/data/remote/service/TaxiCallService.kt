94718a519e5029986cedf8a46bf104bfa1d21443
chosumin432 <01074260518k@gmail.com>
2026-05-20 01:17:13 +0900

    [feat/#39] api 연결 초기 설정

--- app/src/main/java/com/jm/kakaotaxi/data/remote/service/TaxiCallService.kt
+++ app/src/main/java/com/jm/kakaotaxi/data/remote/service/TaxiCallService.kt
@@ -1,5 +1,11 @@
 package com.jm.kakaotaxi.data.remote.service
 
-interface TaxiCallService {
+import com.jm.kakaotaxi.data.remote.dto.BaseResponse
+import com.jm.kakaotaxi.data.remote.dto.T
+import retrofit2.Response
+import retrofit2.http.GET
 
+interface TaxiCallService {
+    @GET("api/v1/rides/{taxi_id}")
+    suspend fun callinfoList(): Response<BaseResponse<T>>
 }
