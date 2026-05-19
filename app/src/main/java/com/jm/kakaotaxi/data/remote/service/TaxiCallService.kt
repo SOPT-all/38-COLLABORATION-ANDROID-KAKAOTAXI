c4802b4636c0397765d64d2f4259e973c04f3169
Hiimynameiss <sh2410152@sookmyung.ac.kr>
2026-05-19 20:42:36 +0900

    [feat/#42] dto, service 추가

--- app/src/main/java/com/jm/kakaotaxi/data/remote/service/TaxiCallService.kt
+++ app/src/main/java/com/jm/kakaotaxi/data/remote/service/TaxiCallService.kt
@@ -1,5 +1,11 @@
 package com.jm.kakaotaxi.data.remote.service
 
-interface TaxiCallService {
+import com.jm.kakaotaxi.data.remote.dto.BaseResponse
+import com.jm.kakaotaxi.data.remote.dto.request.PostTaxiCallRequestDto
+import retrofit2.http.Body
+import retrofit2.http.POST
 
+interface TaxiCallService {
+    @POST("/v1/rides")
+    suspend fun postTaxiCall(@Body request: PostTaxiCallRequestDto): BaseResponse<Unit>
 }
